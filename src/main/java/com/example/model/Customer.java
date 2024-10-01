795d84d4dc966ef262db6aa568b6e57bf4f48b0e
Ryan Fox <s223932052@deakin.edu.au>
2024-10-01 16:18:19 +1000

    Update Customer.java

--- src/main/java/com/example/model/Customer.java
+++ src/main/java/com/example/model/Customer.java
@@ -13,7 +13,7 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String name;
     
@@ -40,7 +40,7 @@ public class Customer {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
