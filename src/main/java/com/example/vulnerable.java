260bf280694520f9899a474688da9f851fb588ac
Ryan Fox <s223932052@deakin.edu.au>
2024-10-06 12:07:21 +1100

    Create vulnerable.java

---
+++ src/main/java/com/example/vulnerable.java
@@ -0,0 +1,19 @@
+package com.exmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class vulnerable extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(vulnerable.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(vulnerable.class, args);
+    }
+}
