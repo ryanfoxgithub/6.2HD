391fd9b4ae24e2e01c5d5f3b99331c07a9873800
Ryan Fox <s223932052@deakin.edu.au>
2024-10-06 19:26:23 +1100

    Update vulnerable.java

--- src/main/java/com/example/vulnerable.java
+++ src/main/java/com/example/vulnerable.java
@@ -1,19 +1,12 @@
-package com.exmaple;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class vulnerable extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(vulnerable.class);
-    }
+public class VulnerableApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(vulnerable.class, args);
+        SpringApplication.run(VulnerableApplication.class, args);
     }
 }
